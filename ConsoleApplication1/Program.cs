1f03b8e77a8aa8f5670437378aaf2490bc283440
andressamarques <andressa.alves.aa.457@gmail.com>
2016-07-29 10:00:09 -0300

    Conversor Decimal p/Binário
    Converte um número decimal para binário.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Conversao_dec_binario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+            Console.Write("Digite um número..:");
+            numero = int.Parse(Console.ReadLine());
+
+            int[] v = new int[32];
+
+            for (int i = 0; i < 32; i++)
+            {
+                v[i] = -1;
+            }
+
+            int index = 0;
+            while (numero > 0)
+            {
+                v[index] = numero % 2;
+                index++;
+                numero = numero / 2;
+            }
+
+            int j;
+
+            for (j = index - 1; j >= 0; j--)
+
+            {
+                Console.Write(" " + v[j]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
